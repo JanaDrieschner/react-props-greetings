bd040a39e6763830f7116cb6f518a3438341a4e6
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-05 13:02:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
-export default function App() {
-  return <h1>Replace me with your component!</h1>;
+export default function App({ name }) {
+  return (
+    <div>
+      <h1> Hello,{"Jana"}</h1>
+    </div>
+  );
 }
